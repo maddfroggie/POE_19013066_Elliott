fb62236833f513db82a29782cda53235bcf30ab8
maddfroggie <56158497+maddfroggie@users.noreply.github.com>
2019-10-28 10:36:52 +0200

    making stuff
    making the assets for buildings and units and im sure there wasnt this many changes but apparently there was.

---
+++ Assets/map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map : MonoBehaviour
+{
+    public GameObject MeleeUnits;
+    public GameObject RangedUnits;
+    public GameObject Wizard;
+    public GameObject RBuildings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
